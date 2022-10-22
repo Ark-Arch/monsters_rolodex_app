acdbb11a89924e8eeb0f2457e2c9bc242a8af987
Agbemuko David Ifeoluwa <davidagbemuko.ad@gmail.com>
2022-10-23 00:42:36 +0100

    added images from robohash.org

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -5,16 +5,18 @@ class CardList extends React.Component{
 
     
     render(){
-
         const {monsters} = this.props
         console.log('render from CardList')
         return(
             <div className='card-list'>
                 {
                     monsters.map(monster => {
+                        const {id, name, email} = monster
                         return(
-                            <div key={monster.id}>
-                                <h1>{monster.name}</h1>
+                            <div className='card-container' key={id}>
+                                <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
+                                <h2>{name}</h2>
+                                <p>{email}</p>
                             </div>
                         )
                     })
