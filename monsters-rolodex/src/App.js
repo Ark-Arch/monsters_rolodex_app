d7d4fc95de6c02320a77358e639684f70b0dfea3
Agbemuko David Ifeoluwa <davidagbemuko.ad@gmail.com>
2022-10-24 07:06:59 +0100

    App Title fix and design

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -50,6 +50,7 @@ class App extends React.Component{
         const filteredMonsters = monsters.filter(monster => monster.name.toLocaleLowerCase().includes(searchField))
         return(
             <div className='App'>
+                <h1 className='app-title'>Monsters Rolodex</h1>
                 <SearchBox className='monsters-search-box' onChangeHandler={onSearchChange} placeholder='search monsters'/>
                 <CardList monsters={filteredMonsters}/>
             </div>
